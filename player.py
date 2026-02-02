7cdb28df94d27a0418637cc9fbec36a7000abcae
Adam Elsayed <adamelsayyed06@gmail.com>
2026-02-02 04:28:11 -0500

    make bet and fold methods

--- player.py
+++ player.py
@@ -1,8 +1,18 @@
 from hand import Hand
 class Player:
     def __init__(self, hand : Hand, chips : int):
-        if money < 0:
+        if chips < 0:
             raise ValueError("Player cannot have negative money")
         self.hand = hand
         self.chips = chips
+
+    def make_bet(self, bet_size : int):
+        chips -= bet_size
+
+    def fold(self):
+        hand = None
     
+    # what are the responsibilities of this class?
+    # i think it makes sense to have it
+    # we can have a game class, goes around checks is the player in the game? 
+    # 
