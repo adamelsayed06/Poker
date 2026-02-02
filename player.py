2073595c7477222c8d192eec24e3cad5a8492b47
Adam Elsayed <adamelsayyed06@gmail.com>
2026-02-02 04:24:18 -0500

    rename money to chips

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
 from hand import Hand
 class Player:
-    def __init__(self, hand : Hand, money : int):
+    def __init__(self, hand : Hand, chips : int):
         if money < 0:
             raise ValueError("Player cannot have negative money")
         self.hand = hand
-        self.money = money
+        self.chips = chips
     
