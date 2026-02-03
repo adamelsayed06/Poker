4f331f635076ad349320ebf2c17e37863c7aa600
Adam Elsayed <adamelsayyed06@gmail.com>
2026-02-03 18:33:30 -0500

    handle bet logic

--- main.py
+++ main.py
@@ -39,34 +39,30 @@ def cards_to_readable_output(cards : List[Card]) -> str:
 def handle_bet(player : Player, player_amount_at_stake : int, amount_needed_to_be_put_at_stake : int):
     if player_amount_at_stake == -1:
         player.fold()
-        return 0
     else:
-
-    # read the input from the user.
-    # also make sure to remove chips from the player here since once its at stake u cant take ur bet back
-    # if -1, fold
-    # if they input some amount, let's check if its enough by comparing it to the min_bet.
-    # min_bet represents the TOTAL amount u need to put at stake, so add how much they have at stake with the number they input to min_bet
-    # if its < error, if its == round ends nothing happens it just moves on to either the end or next person depending on who goes
-    # if its > its a raise so min_bet changes and the other person gets the chance to reraise
+        if player_amount_at_stake < amount_needed_to_be_put_at_stake:
+            print("Error: You did not put enough money in... folding your hand") # refactor?
+            player.fold()
+        elif player_amount_at_stake == amount_needed_to_be_put_at_stake:
+            print("Bet called / checked")
+        else:
+            print("Bet raised")
 
 def make_betting_decision(player1 : Player, player2 : Player) -> int:
-    potIncrement = 0
-
     min_bet = 0
     player_1_amount_at_stake = 0
     player_2_amount_at_stake = 0
 
     while True: # emulates a do-while loop
         player_1_amount_at_stake = int(input("Player 1, input your TOTAL bet size: (0 to check, -1 to fold): "))
-        potIncrement += handle_bet(player1, player_1_amount_at_stake, min_bet)
+        handle_bet(player1, player_1_amount_at_stake, min_bet)
 
         player_2_amount_at_stake = int(input("Player 2, input your TOTAL bet size: (0 to check, -1 to fold)"))
-        potIncrement += handle_bet(player2, player_2_amount_at_stake, min_bet)
+        handle_bet(player2, player_2_amount_at_stake, min_bet)
         if player_1_amount_at_stake == player_2_amount_at_stake:
             break        
 
-    return potIncrement
+    return player_1_amount_at_stake + player_2_amount_at_stake
 
 def main():
     # Poker will be heads-up (two players) -> later we can pass in a number of players and implement that
@@ -104,6 +100,8 @@ def main():
         print(f"River: {cards_to_readable_output(river)}")
         
         pot += make_betting_decision(player1, player2)
+
+        determine_winner_and_payout_pot(player1, player2, pot)
 
 if __name__ == "__main__":
     main()
