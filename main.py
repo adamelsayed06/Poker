1c3aaa84db9239c0444a5b166b353d75ac6c5aca
Adam Elsayed <adamelsayyed06@gmail.com>
2026-02-02 19:00:06 -0500

    initialize deck and deal players 2 cards from the deck

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from player import Player
+from deck import Deck
 
 def main():
     # Poker will be heads-up (two players)
     player1 = Player(100)
     player2 = Player(100)
+
+    while player1.chips > 0 and player2.chips > 0:
+        deck = Deck()
+
+        player1.deal_hand(deck.pick_card, deck.pick_card)
+        player2.deal_hand(deck.pick_card, deck.pick_card)
+
 
 if __name__ == "__main__":
     main()
