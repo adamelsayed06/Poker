c88342f8234531fa0ef4482c6ca1dcb9d9e7e44c
Adam Elsayed <adamelsayyed06@gmail.com>
2026-02-02 19:40:21 -0500

    testing player's hands

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from deck import Deck
 from hand import Hand
 from card import Card
 
-def hand_to_readable_output(self, card : Card) -> str:
+def card_to_readable_output(self, card : Card) -> str:
     if card.suit == 0:
         return (f"{card.rank}♣")
     elif card.suit == 1:
@@ -24,8 +24,8 @@ def main():
         player1.deal_hand(deck.pick_card, deck.pick_card)
         player2.deal_hand(deck.pick_card, deck.pick_card)
 
-        print("Player 1's Hand is: " + player1.hand)
-        print("Player 2's hand is: " + player2.hand)
+        print("Player 1's Hand is: " + card_to_readable_output(player1.hand.first_card) + " " + card_to_readable_output(player1.hand.second_card))
+        print("Player 2's hand is: " + card_to_readable_output(player2.hand.first_card) + " " + card_to_readable_output(player2.hand.second_card))
 
 
 if __name__ == "__main__":
