fc8d957e53faea8bbd421a9022b4faee946b03d2
Adam Elsayed <adamelsayyed06@gmail.com>
2026-02-02 22:09:40 -0500

    make betting decision business logic?

--- main.py
+++ main.py
@@ -32,11 +32,34 @@ def card_to_readable_output(card : Card) -> str:
 
     return (f"{cleaned_rank}{cleaned_suit}")
 
-def make_betting_decision():
+def make_betting_decision(player1 : Player, player2 : Player):
+    # TODO: refactor this and sketch out logic
+    #logic flow -> lets have amt at stake for p1 & p2
+
+    # if currPlayer amt at stake < max(p1 amt at stake, p2 amt at stake) -> they need to bet or fold, otherwise check or bet
+    # they each get 1 turn, but if a person raises it resets?
+    # when both turns are taken the function can end
+
+    min_bet = 0
+
+    player_1_amount_at_stake = 0
+    player_2_amount_at_stake = 0
+
+    player_1_bet_size = int(input("Player 1, input your bet size: (0 to check): "))
+    min_bet = max(min_bet, player_1_bet_size)
+
+    player_2_bet_size = int(input("Player 2, input your bet size: (-1 to fold)"))
+    if player_2_bet_size == -1:
+        player2.fold()
+    if player_2_bet_size < min_bet:
+        print(f"Bet size too low, must input at least {min_bet} to bet")
+    
+
+
     time.sleep(3)
 
 def main():
-    # Poker will be heads-up (two players)
+    # Poker will be heads-up (two players) -> later we can pass in a number of players and implement that
     player1 = Player(100)
     player2 = Player(100)
 
